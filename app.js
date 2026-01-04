55773728f5cc0043d1222e6ab80abef4f262fb9e
Rohan85041 <chrohankumar85041@gmail.com>
2026-01-04 10:41:03 +0530

    products services are added

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express=require('express');
 const app=express();
 app.use(express.json())
+let statusrouter=require('./src/routes/status.routes')
+let factrouter=require('./src/routes/fact.routes')
+let productrouter=require('./src/routes/product.route')
+app.use('/',statusrouter)
+app.use('/api',factrouter)
+app.use('/v2',productrouter)
 module.exports=app
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 require('dotenv').config();
 let app=require('./app')
 let connection=require('./src/config/db.config')
-let statusrouter=require('./src/routes/status.routes')
-let factrouter=require('./src/routes/fact.routes')
-app.use('/',statusrouter)
-app.use('/api',factrouter)
+
 const port=process.env.PORT
 app.listen(port,()=>{
     console.log(`server is running on ${port}`)
